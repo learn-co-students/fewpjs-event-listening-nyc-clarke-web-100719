c1ecb778f25f557b8aadb35c27770cd1fd94c52e
jastaffen <jack.staffen@gmail.com>
2019-11-19 13:40:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    let input = document.getElementById('input');
+    input.addEventListener('click', function(event){
+        alert('I was clicked!');
+    });
 }
+
+let input = document.getElementById('input');
+
+addingEventListener();
